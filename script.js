7c4791603f4f87ac915ef73064f0390f8fd1a883
SyedYasirAli03 <yasirsyedali003@gmail.com>
2024-09-27 17:48:43 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// scripts.js
+
+function loginClicked() {
+    window.location.href = 'login.html';
+}
+
+function signupClicked() {
+    window.location.href = 'signup.html';
+}
+
+function createQuizClicked() {
+
+    window.location.href = 'CreateQuizBanner.html';
+}
+
+function cancelClicked() {
+    // Add your logic for cancel button
+    alert("Cancel clicked");
+}
+
+function saveNextClicked() {
+    window.location.href = 'CreateQuestions.html';
+}
+function editQuizClicked() {
+
+    alert("Edit Quiz button clicked");
+
+}
+function joinQuizClicked() {
+    // Add logic for join quiz
+    alert("Join Quiz button clicked");
+}
+
+function startQuizClicked() {
+    // Add logic for start quiz
+    alert("Start Quiz button clicked");
+}
+
+function searchClicked() {
+    // Add logic for search
+    alert("Search button clicked");
+}
